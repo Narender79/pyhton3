bec3d367615ee4d2215032da51b8928d9817ad77
Narender <113188277+Narender79@users.noreply.github.com>
2022-10-30 11:35:54 +0530

    Add files via upload

---
+++ Operation.py
@@ -0,0 +1,17 @@
+nameoflist=["hello","hey","this","IS","List"]
+# append
+nameoflist.append("name")
+print(nameoflist)
+# insert
+nameoflist.insert(4,"name")
+print(nameoflist)
+# pop
+nameoflist.pop()
+print(nameoflist)
+# delete
+del nameoflist[2,4]
+print(nameoflist)
+# clear
+nameoflist.clear()
+
+
